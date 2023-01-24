b477c2a0343a921ce8958728e120941a34a36ba4
“Meija <mlohiniva@hotmail.com>
2023-01-24 18:29:36 +0200

    firstIf added

---
+++ 20230124/firstIf/main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    // Create a variable for entered number
+    int enteredNumber;
+    // Prompt user to enter a number.
+    printf("Please enter a number: \n");
+    // Read user input
+    scanf("%d", &enteredNumber);
+    // Check if entered number is 0
+    if ( enteredNumber == 0 ) {
+        // If yes -> print out "You entered a zero".
+        printf("\n You entered a zero.");
+    } else {
+        // If no -> print out "You entered something else".
+        printf("\nYou entered something else.");
+    }
+    return 0;
+}
