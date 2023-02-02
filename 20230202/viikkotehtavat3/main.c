0d7a10ef357021f0a181b3f2a5414b4d68721795
“Meija <mlohiniva@hotmail.com>
2023-02-02 19:15:36 +0200

    Added viikkotehtava 3.2

--- 20230202/viikkotehtavat3/main.c
+++ 20230202/viikkotehtavat3/main.c
@@ -24,5 +24,31 @@ vaihto). Käytä toistorakennetta. */
         printf("100 ");
         i++;
     }
+
+    /*
+     * Tee C-kielinen ohjelma joka lukee kysyy käyttäjän nimen ja sen montako kertaa se tulostetaan näytölle. Sitten tulostaa nimen näytölle em. kertaa. Tee ratkaisu
+        a) while toistolla
+        b) do-while toistolla
+        c) for-toistolla
+(HUOM! voit tehdä, jos haluat a)-c) kohdat samaan ohjelmaan peräkkäin näin sinun ei
+tarvitse tehdä kolmea erillistä ohjelmaa)
+*/
+    // Luodaan muuttuja nimelle
+    char nimi[15];
+    // Luodaan muuttuja toistokerroille
+    int toistokerrat;
+    // Pyydä nimi
+    printf("\nNimi: ");
+    // Lue nimi
+    scanf("%s", nimi);
+    // Pyydä toistokerrat
+    printf("Toistokerrat: ");
+    // Lue toistokerrat
+    scanf("%d", &toistokerrat);
+    // Toista niin monta kertaa kuin pyydettiin
+    for ( int i = 0; i < toistokerrat; i++ ) {
+        // Tulosta nimi
+        printf("\n%s", nimi);
+    }
     return 0;
 }
