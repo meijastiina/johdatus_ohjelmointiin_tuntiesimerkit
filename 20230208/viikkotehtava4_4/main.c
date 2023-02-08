4006e741f2d174c5019e31f6b3e1092ff36c577c
“Meija <mlohiniva@hotmail.com>
2023-02-08 19:32:24 +0200

    Added viikkotehtava 4.4

--- 20230208/viikkotehtava4_4/main.c
+++ 20230208/viikkotehtava4_4/main.c
@@ -1,13 +1,38 @@
 /*
- * Tee ohjelma jossa on pääohjelma (main-funktio) sekä yksi funktio eli aliohjelma, joka kysyy
-float-luvun käyttäjältä, kertoo luvun kolmella ja palauttaa tuloksen main()-funktiolle, jossa
-tulos tulostetaan näytölle.
+ * Tee ohjelma jossa on pääohjelma (main-funktio) sekä
+ * yksi funktio eli aliohjelma, joka
+ *      kysyy float-luvun käyttäjältä,
+ *      kertoo luvun kolmella
+ *      ja palauttaa tuloksen main()-funktiolle,
+ * jossa tulos tulostetaan näytölle.
 */
 
 #include <stdio.h>
 
+// Funktion prototyyppi
+float multiplyByThree();
 int main()
 {
-    printf("Hello World!\n");
+    // Kutsu funktiota ja tulos tulostetaan näytölle
+    printf("\nAntamasi luku kertaa kolme on %.2f", multiplyByThree());
     return 0;
+}
+/*
+ * funktio eli aliohjelma, joka
+ *      kysyy float-luvun käyttäjältä,
+ *      kertoo luvun kolmella
+ *      ja palauttaa tuloksen main()-funktiolle,
+ * Parametrit:  -
+ * Paluuarvo:   float - Luku kerrottuna kolmella
+ */
+float multiplyByThree()
+{
+    // kysyy float-luvun käyttäjältä,
+    printf("\nFloat: ");
+    float givenNumber;
+    scanf("%f", &givenNumber);
+    // kertoo luvun kolmella
+    // ja palauttaa tuloksen main()-funktiolle,
+    return givenNumber * 3;
+
 }
