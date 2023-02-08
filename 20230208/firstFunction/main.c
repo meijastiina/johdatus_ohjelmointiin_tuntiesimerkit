9788624b4d1a1c23031a11cb4e783814ca2c1292
“Meija <mlohiniva@hotmail.com>
2023-02-08 17:43:55 +0200

    Added examples

--- 20230208/firstFunction/main.c
+++ 20230208/firstFunction/main.c
@@ -9,9 +9,23 @@ void printHello() {
     printf("Hello!\n");
 }
 
+// Create function that sums two numbers given as input parameters
+// Input parameters:    int number1
+//                      int number2
+// Return value: void (none)
+void sum(int number1, int number2) {
+    // Function body
+    // Print out sum of given numbers
+    printf("%d + %d = %d\n", number1, number2, number1 + number2);
+}
 int main()
 {
     // Call function printHello()
     printHello();
+    // Call function sum()
+    sum(5, 8);
+    sum(35, 48);
+    sum(5543, 8234);
+    sum(5, 84);
     return 0;
 }
