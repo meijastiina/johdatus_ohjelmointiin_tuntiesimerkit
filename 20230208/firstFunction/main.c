b64228d2028410530bc0de3f6abe4c92540b750a
“Meija <mlohiniva@hotmail.com>
2023-02-08 17:34:47 +0200

    Added examples

---
+++ 20230208/firstFunction/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+// Create function that prints out Hello
+// Input parameters: -
+// Return value: void (none)
+void printHello() {
+    // Function body
+    // Print out Hello
+    printf("Hello!\n");
+}
+
+int main()
+{
+    // Call function printHello()
+    printHello();
+    return 0;
+}
