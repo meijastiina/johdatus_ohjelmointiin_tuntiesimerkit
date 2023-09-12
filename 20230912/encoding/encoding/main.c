58a4e36c0429e8fc931ce39228098bf21de3b553
“Meija <mlohiniva@hotmail.com>
2023-09-12 16:03:10 +0300

    Set locale example updated

--- 20230912/encoding/encoding/main.c
+++ 20230912/encoding/encoding/main.c
@@ -5,5 +5,21 @@ int main()
 {
     setlocale(LC_ALL,"");
     printf("Heipä hei maailma!\n");
+
+    char merkki;
+    printf("Haluatko jatkaa (y/n)?");
+    scanf("%c", &merkki);
+    switch(merkki)
+    {
+        case 'y':
+            printf("Jatketaan\n");
+            break;
+        case 'n':
+            printf("Lopetetaan\n");
+            break;
+        default:
+            printf("Virheellinen valinta\n");
+    }
+
     return 0;
 }
