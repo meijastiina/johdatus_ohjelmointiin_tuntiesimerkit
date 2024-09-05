f12af7e339d5bbea1a2096be764d9296bd565f6a
Meija Lohiniva <meija.lohiniva@oamk.fi>
2024-09-05 13:44:51 +0300

    ekat harkat

--- 2024autumn/20240905/pm/viikkotehtava-1/main.c
+++ 2024autumn/20240905/pm/viikkotehtava-1/main.c
@@ -24,6 +24,27 @@ int main()
     printf("\nnumber2 value is %.2f\n", number2);
     printf("\ncharacter1 value is %c\n", character1);
 
+    /******* TASK 2 ****************
+     * Tee C-kielinen ohjelma, joka muuntaa antamasi
+     * lämpötilan (Celsius asteina)
+     * Farenheit -asteiksi.
+     * Tulostusesimerkki:
+     * Antamasi lämpötila __ on Farenheit asteina __.
+     * ***************************/
+    // Tulosta otsikko
+    printf("\n****** TASK 2 ****************\n");
+    // luo muuttujat
+    float temperatureInCelcius;
+    float temperatureInFahrenheit;
+    // pyydä käyttäjää syöttämään lämpötila celsius asteina
+    printf("Temperature in Celsius: ");
+    // lue käyttäjän syöte (tallenna muuttujaan)
+    scanf("%f", &temperatureInCelcius);
+    // muunna annettu lämpötila Fahrenheiteiksi (0°C × 9/5) + 32 = 32°F
+    temperatureInFahrenheit = (temperatureInCelcius * 9/5) + 32;
+    // tulosta
+    printf("Temperature you entered (%.1f C) is %.1f in Fahrenheit.", temperatureInCelcius, temperatureInFahrenheit);
+
     // Päättää ohjelman suorituksen
     return 0;
 }
