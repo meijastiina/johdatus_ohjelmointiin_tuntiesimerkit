b451af5d0c01a34afeb8ff377a37893023c82bbd
Meija Lohiniva <meijastiina@gmail.com>
2025-09-02 14:59:21 +0300

    2.9. tokaharkkojen koodit lämpötila

--- 20250902b/main.c
+++ 20250902b/main.c
@@ -8,5 +8,15 @@
 
 int main()
 {
-    return 0;
+  // Luodaan muuttujat
+  int temperatureInCelcius;
+  // Tulostetaan käyttäjälle pyyntö syöttää lämpötila celsius-asteina
+  printf("Temperature in celsius: \n");
+  // Luetaan käyttäjän syöte muuttujaan
+  scanf("%d", &temperatureInCelcius);
+  // Muutetaan celsiukset fahrenheiteiksi. Kaava: celsius * 1,8 + 32
+  float temperatureInFarenheit = temperatureInCelcius * 1.8 + 32;
+  // Tulostetaan näytölle lopputulos.
+  printf("%d Celsius is %.1f in Farenheit", temperatureInCelcius, temperatureInFarenheit);
+  return 0;
 }
